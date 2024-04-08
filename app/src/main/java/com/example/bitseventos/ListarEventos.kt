e7584ce1c666ed9383715d36832d6a26e8630ca9
Matheus Pereira <matheusgoushinsoo@gmail.com>
2024-04-08 18:31:49 -0300

    add: listagem de eventos

---
+++ app/src/main/java/com/example/bitseventos/ListarEventos.kt
@@ -0,0 +1,11 @@
+package com.example.bitseventos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListarEventos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listar_eventos)
+    }
+}
