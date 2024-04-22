0f19dadd335c19ff19ce86d5fe6fe80c17987bf6
Matheus Pereira <matheusgoushinsoo@gmail.com>
2024-04-22 16:31:48 -0300

    add: function to register a user for an event

--- app/src/main/java/com/example/bitseventos/ListarEventos.kt
+++ app/src/main/java/com/example/bitseventos/ListarEventos.kt
@@ -2,10 +2,19 @@ package com.example.bitseventos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class ListarEventos : AppCompatActivity() {
+    private lateinit var viewEventButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_listar_eventos)
+
+        viewEventButton = findViewById(R.id.ViewEvent)
+
+        viewEventButton.setOnClickListener {
+        changeToAnotherScreen(this, VisualizarEvento::class.java)
+        }
     }
 }
